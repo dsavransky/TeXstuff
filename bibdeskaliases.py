ba51dc450500f74cde8eb873b5ab4002396bc44b
Dmitry Savransky <dsavransky@gmail.com>
2024-09-16 13:15:09 -0400

    fixing corrupted main bib and updating bibdeskaliases code

--- bibdeskaliases.py
+++ bibdeskaliases.py
@@ -4,21 +4,28 @@ import base64
 import glob
 import numpy as np
 
-mainfile = os.path.join(os.environ['HOME'],'Documents','TexStuff','Main.bib')
-paperpath = os.path.join(os.environ['HOME'],'Documents','Papers')
+from Foundation import NSPropertyListSerialization, NSData
+
+
+mainfile = os.path.join(os.environ["HOME"], "Documents", "TexStuff", "Main.bib")
+paperpath = os.path.join(os.environ["HOME"], "Documents", "Papers")
 
 with open(mainfile) as f:
     bib = f.read()
 
-aliases = re.findall(r"Bdsk-File-\d = {(\S*)}",bib)
+aliases = re.findall(r"bdsk-file-\d = {(\S*)}", bib)
 
 files = []
 for a in aliases:
     tmp = base64.b64decode(a.encode())
-    files.append(os.path.join(paperpath,re.search(b"Documents/Papers/(.+?)(?=\x00)",tmp).groups()[0].decode()))
+    files.append(
+        os.path.join(
+            paperpath, re.search(b"Papers/(.+?)(?=O\x11)", tmp).groups()[0].decode()
+        )
+    )
 
 
-diskfiles = glob.glob(os.path.join(paperpath,'*.*'))
+diskfiles = glob.glob(os.path.join(paperpath, "*.*"))
 
 
 set(files) - set(diskfiles)
@@ -27,13 +34,15 @@ missing = list(set(diskfiles) - set(files))
 np.sort(missing)
 
 
-
-
-#for when pyobjc is fixed:
-from Foundation import NSPropertyListSerialization, NSData
-decodedBytes = base64.b64decode(a)
-nsData = NSData.dataWithBytes_length_(decodedBytes, len(decodedBytes))
-plist, fmt, error = NSPropertyListSerialization\
-    .propertyListFromData_mutabilityOption_format_errorDescription_(
-        nsData, 0, None, None)
-
+files = []
+for a in aliases:
+    decodedBytes = base64.b64decode(a)
+    nsData = NSData.dataWithBytes_length_(decodedBytes, len(decodedBytes))
+    (
+        plist,
+        fmt,
+        error,
+    ) = NSPropertyListSerialization.propertyListFromData_mutabilityOption_format_errorDescription_(
+        nsData, 0, None, None
+    )
+    files.append(os.path.join(paperpath, os.path.split(plist["relativePath"])[-1]))
